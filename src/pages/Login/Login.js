933dff2a261f3d50c35dd35c83055d5805746b98
VLKH3 <knowledgehut@vernlam.com>
2023-03-23 12:07:23 -0700

    temp

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,53 +1,64 @@
-import React, { useContext, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import Rating from '../../shared/components/Rating';
-import { UserDataContext } from '../../shared/data';
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserDataContext } from "../../shared/data";
+import { TextField, Button, Rating } from "@mui/material";
+import { useForm } from "react-hook-form";
 
 export default function Login() {
+  const { register, handleSubmit } = useForm();
 
-    const [username, setUsername] = useState('');
-    const [password, setPassword] = useState('');
-    const [value, setValue] = useState(0);
-    const [value2, setValue2] = useState(0);
-    const [passwordType, setPasswordType] = useState('password');
-    
-    const { setUsernameGlobal } = useContext(UserDataContext)
+  const [value, setValue] = useState(0);
+  const [value2, setValue2] = useState(0);
+  const [passwordType, setPasswordType] = useState("password");
 
-    const navigate = useNavigate()
+  const { setUsernameGlobal } = useContext(UserDataContext);
 
-    function loginClick(e) {
-        if(username == "Sagar" && password == "123"){
-            setUsernameGlobal("Sagar")
-            navigate('/home')
-        }else{
-            setUsernameGlobal("")
-        }
+  const navigate = useNavigate();
+
+  function loginClick(formObj) {
+    if (formObj.username == "vern" && formObj.password == "123") {
+      setUsernameGlobal("vern");
+      navigate("/home");
+    } else {
+      setUsernameGlobal("");
     }
+  }
 
-    function clearUsername(){
-        if(passwordType == 'password'){
-            setPasswordType('text')
-        }else{
-            setPasswordType('password')
-        }
+  function clearUsername() {
+    if (passwordType == "password") {
+      setPasswordType("text");
+    } else {
+      setPasswordType("password");
     }
+  }
 
-    function apply(){
-        setValue2(value)
-    }
+  function apply() {
+    setValue2(value);
+  }
 
-    return (
-        <div>
-            <div className='login-form'>
-                {/* <input value={value} onChange={e=>setValue(e.target.value)}></input>
-                <button onClick={apply}>Apply</button>
-                <Rating value={value2}/> */}
-                <button onClick={clearUsername}>Show/Hide Password</button>
-                <input value={username} onChange={(e) => setUsername(e.target.value)} type={'text'}></input>
-                <input value={password} onChange={e => setPassword(e.target.value)} type={passwordType}></input>
-                <button onClick={loginClick} type='button'>Login</button>
-            </div>
-        </div>
-    )
+  return (
+    <div>
+      <div className="login-form">
+        <form onSubmit={handleSubmit(loginClick)}> 
+          <button onClick={clearUsername}>Show/Hide Password</button>
+          <TextField
+          {...register('username')}
+            id="outlined-basic"
+            label={"Username"}
+            type={"text"}
+          ></TextField>
+          <TextField
+          {...register('password')}
+            id="outlined-basic"
+            label={"Password"}
+            type={passwordType}
+          ></TextField>
+          <Button variant="contained" onClick={loginClick}>
+            Login
+          </Button>
+          <Rating value={3} />
+        </form>
+      </div>
+    </div>
+  );
 }
-
